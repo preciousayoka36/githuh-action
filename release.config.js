95c543403720487862ef82952db314bab924a581
preciousayoka36 <preciousayoka924@gmail.com>
2023-03-22 22:22:00 -0400

    fix: sematic versioning

---
+++ release.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    branches: "main",
+    repositoryUrl: "https://github.com/preciousayoka36/githuh-action.git",
+    plugins: [
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+      '@semantic-release/git',
+      '@semantic-release/github']
+ }
+ 
